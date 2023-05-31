cdd4cba1c736866538735d07c216c742090df733
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-31 15:03:15 +0200

    create cartelle img e styles e inserito css generale

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <template>
   <HelloWorld />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@use './styles/partials/variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
